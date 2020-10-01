69fcbed5af6a0add155a629fa9e87628be1b9fe8
Apurv <apurvsingla@gmail.com>
2020-10-01 09:45:42 +0530

    canvas

---
+++ src/Component/Button/Button.js
@@ -0,0 +1,33 @@
+import React, { Component, Fragment } from 'react';
+import ScreenCapture from '../ScreenCapture/ScreenCapture';
+import './Button.css';
+
+class Button extends Component {
+  state = {
+    screenCapture: ''
+  }
+
+  handleScreenCapture = (screenCapture) => {
+    this.setState({
+      screenCapture
+    })
+  }
+
+  render() {
+    const { screenCapture } = this.state
+    return (
+      <div className="button">
+      <ScreenCapture onEndCapture={this.handleScreenCapture}>
+        {({ onStartCapture }) => (
+          <Fragment>
+            <button onClick={onStartCapture}>Capture</button>
+            <img src={screenCapture} alt=""/>
+          </Fragment>
+        )}
+      </ScreenCapture>
+      </div>
+    );
+  }
+}
+
+export default Button;
